0668345b63089c07f5c56f53003c78433b4b8fdd
MauricioVazquezM <98365606+MauricioVazquezM@users.noreply.github.com>
2022-08-25 14:43:45 -0500

    Add files via upload

---
+++ VISUALISATION/SEC3_GAPMINDER.R
@@ -0,0 +1,118 @@
+### GAPMINDER DATASET
+# load and inspect gapminder data
+library(dslabs)
+data(gapminder)
+head(gapminder)
+
+# compare infant mortality in Sri Lanka and Turkey
+x <- gapminder %>% 
+  filter(year==2015 & country %in% c("Sri Lanka", "Turkey")) %>%
+  select(country, infant_mortality)
+x
+
+
+
+### LIFE EXPECTANCY AND FERTILITY RATES
+# basic scatterplot of life expectancy versus fertility
+ds_theme_set()    # set plot theme
+filter(gapminder, year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy)) +
+  geom_point()
+
+# add color as continent
+filter(gapminder, year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy, color = continent)) +
+  geom_point()
+
+
+
+### FACETING
+# facet by continent and year
+filter(gapminder, year %in% c(1962, 2012)) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_grid(continent ~ year)
+
+# facet by year only
+filter(gapminder, year %in% c(1962, 2012)) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_grid(. ~ year)
+
+# facet by year, plots wrapped onto multiple rows
+years <- c(1962, 1980, 1990, 2000, 2012)
+continents <- c("Europe", "Asia")
+gapminder %>%
+  filter(year %in% years & continent %in% continents) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_wrap(~year)
+
+
+
+### TIME SERIES PLOTS
+## single time series
+# scatterplot of US fertility by year
+gapminder %>%
+  filter(country == "United States") %>%
+  ggplot(aes(year, fertility)) +
+  geom_point()
+
+# line plot of US fertility by year
+gapminder %>%
+  filter(country == "United States") %>%
+  ggplot(aes(year, fertility)) +
+  geom_line()
+
+## multiple time series
+# line plot fertility time series for two countries- only one line (incorrect)
+countries <- c("South Korea", "Germany")
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility)) +
+  geom_line()
+
+# line plot fertility time series for two countries - one line per country
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility, group = country)) +
+  geom_line()
+
+# fertility time series for two countries - lines colored by country
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility, col = country)) +
+  geom_line()
+
+## adding text labels to a plot
+# life expectancy time series - lines colored by country and labeled, no legend
+labels <- data.frame(country = countries, x = c(1975, 1965), y = c(60, 72))
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, life_expectancy, col = country)) +
+  geom_line() +
+  geom_text(data = labels, aes(x, y, label = country), size = 5) +
+  theme(legend.position = "none")
+
+
+
+### TRANSFORMATIONS
+# add dollars per day variable
+gapminder <- gapminder %>%
+  mutate(dollars_per_day = gdp/population/365)
+
+# histogram of dollars per day
+past_year <- 1970
+gapminder %>%
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(dollars_per_day)) +
+  geom_histogram(binwidth = 1, color = "black")
+
+# repeat histogram with log2 scaled data
+gapminder %>%
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(log2(dollars_per_day))) +
+  geom_histogram(binwidth = 1, color = "black")
+
+# repeat histogram with log2 scaled x-axis
+gapminder %>%
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(dollars_per_day)) +
+  geom_histogram(binwidth = 1, color = "black") +
+  scale_x_continuous(trans = "log2")
